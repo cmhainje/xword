cc3da3548e155a2d241cb1a2f33b1ee6a9cbc2fb
Connor Hainje <cmhainje@gmail.com>
2020-10-02 20:40:39 -0400

    Bug fix in server js

--- server.js
+++ server.js
@@ -12,8 +12,9 @@
 
 const PORT = process.env.PORT || 8000
 
-const express = require('express')();
+const express = require('express');
 const app = express();
+const path = require('path');
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
